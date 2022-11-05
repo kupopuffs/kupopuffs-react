ca4ccd29997b742616db46e6b0d3dde0c37bbc43
Jonathan Pham <pjonathanh@gmail.com>
2022-11-05 12:24:29 -0700

    milestone

--- src/App.js
+++ src/App.js
@@ -2,24 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="kupo.png" className="App-logo" alt="logo" />
+                <h1>
+                    Jonathan Pham
+                </h1>
+                <h2>
+                    (aka @kupopuffs)
+                </h2>
+                <h2>
+                    pjonathanh (at) gmail.com
+                </h2>
+                <hr />
+                <p>
+                    check out my
+                </p>
+                <div className="socials-container">
+                    <span className="socials-col">
+                        <a href="https://github.com/kupopuffs" target="_blank" alt="github-link">
+                            <img className="socials-logo" src="github.png" alt="github-logo" />
+                            <p>
+                                code
+                            </p>
+                        </a>
+
+                    </span>
+                    <span className="socials-col">
+                        <a href="https://www.linkedin.com/in/jonathan-pham-18037797/" target="_blank" alt="linkedin-link">
+                            <img className="socials-logo" src="linkedin.png" alt="linkedin-logo" />
+                            <p>
+                                work
+                            </p>
+                        </a>
+                    </span>
+                    <span className="socials-col">
+                        <a href="https://www.instagram.com/kupopharts" target="_blank" alt="instagram-link">
+                            <img className="socials-logo" src="instagram.png" alt="instagram-logo" />
+                            <p>
+                                art
+                            </p>
+                        </a>
+                    </span>
+                </div>
+
+                <a className="privacy" href="privacy.txt" target="_blank" alt="privacy-link">
+                    privacy.txt
+                </a>
+            </header>
+
+        </div>
+    );
 }
 
 export default App;
